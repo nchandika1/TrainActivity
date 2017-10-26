46f9ec25b8107d14c85904e8bf4f7875d3ee5c4b
nchandika1 <nchandika@gmail.com>
2017-10-25 20:31:14 -0700

    added updating by the minute code

--- train.js
+++ train.js
@@ -21,6 +21,44 @@ MAIN LOGIC FOR POPULATING FIRE BASE AND THE WEBPAGE WITH LATEST TRAIN INFORMATIO
   //Get ref to the database
   var database = firebase.database();
 
+  // Variables to udpate Train Table by the minute
+  var tableUpdate = 5000; // 60 seconds
+  var tableUpdateTimer;
+  var trainFttArray = [];
+
+  // Update the table Next Arrival and Minutes away by the minute 
+  function tableUpdateFunction() {
+
+    var numRows = $("table").children()
+        .eq(1) // tbody
+        .children("tr").length;  //  # of rows
+        
+    // For each Row    
+    for (var i=0; i< numRows; i++) {
+      // Get the array of Tds for each row
+      var trainRowTds = $("table").children().eq(1).children("tr").eq(i).children("td");
+      var name = trainRowTds.eq(0).text();
+
+      // Get the corresponding first Train Time from the saved array
+      // There is a better way to do it to improve performance but this works for now
+      for (var i=0; i<trainFttArray.length; i++) {
+        if (name == trainFttArray[i][0]) {
+          var freq = trainRowTds.eq(2).text();
+          var nextTrainInfo = nextArrivalTime(trainFttArray[i][1], freq);
+          // Use moment JS to display the Next Arrival time in the AM/PM format
+          var arrivalTime = moment(moment(nextTrainInfo[0], 'HH:mm')).format("hh:mm A")
+          trainRowTds.eq(3).text(arrivalTime);
+          trainRowTds.eq(4).text(nextTrainInfo[1]);
+          break;
+        }
+      }
+    }
+
+    // Clear and Restart the timer
+    clearTimeout(tableUpdateTimer);
+    tableUpdateTimer = setTimeout(tableUpdateFunction, tableUpdate); // Restart timer
+  }
+
   // This function retrieves database snapshot from the firebase
   // Add populate Table on the webpage with the database entries
   function populateTable() {
@@ -35,11 +73,17 @@ MAIN LOGIC FOR POPULATING FIRE BASE AND THE WEBPAGE WITH LATEST TRAIN INFORMATIO
     trainRef.once('value', function(snapshot) {
       snapshot.forEach(function(childSnapshot) {
         var childData = childSnapshot.val();
+        var trainInfo = [];
+        trainInfo[0] = childData.name;
+        trainInfo[1] = childData.ftt;
+        trainFttArray.push(trainInfo);
 
         // Create Table Rows now
         addRowElement(childData.name, childData.destination, childData.ftt, childData.frequency);
       });
     });
+
+    tableUpdateTimer = setTimeout(tableUpdateFunction, tableUpdate);
   }
 
   // This function populates the table rows on the web page
@@ -50,12 +94,14 @@ MAIN LOGIC FOR POPULATING FIRE BASE AND THE WEBPAGE WITH LATEST TRAIN INFORMATIO
     // Calculate the Next Train and Minutes Away dynamically
     nextTrainInfo = nextArrivalTime(ftt, frequency);
 
+    // Use moment JS to display the Next Arrival time in the AM/PM format
+    var arrivalTime = moment(moment(nextTrainInfo[0], 'HH:mm')).format("hh:mm A");
     var row = $("<tr>");
     row.attr('id', "table-row");
     var markup = "<td>" + name + "</td>" +
                   "<td>" + destination + "</td>" +
                   "<td>" + frequency + "</td>" +
-                  "<td>" + nextTrainInfo[0] + "</td>" +
+                  "<td>" + arrivalTime + "</td>" +
                   "<td>" + nextTrainInfo[1] + "</td>";
     row.html(markup);          
     $("#table-body").append(row);
@@ -96,7 +142,14 @@ MAIN LOGIC FOR POPULATING FIRE BASE AND THE WEBPAGE WITH LATEST TRAIN INFORMATIO
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    // Create Table Rows now
+    // Save First Train Time in this local array
+    // We use this to refresh the table cells every minute
+    var trainInfo = [];
+    trainInfo[0] = trainName;
+    trainInfo[1] = firstTrainTime;
+    trainFttArray.push(trainInfo);
+
+    // Create Table Rows
     addRowElement(trainName, trainDest, firstTrainTime, trainFreq);
 
     // Clear the form
@@ -105,6 +158,7 @@ MAIN LOGIC FOR POPULATING FIRE BASE AND THE WEBPAGE WITH LATEST TRAIN INFORMATIO
     $("#first-train-time").val("");
     $("#frequency").val("");
   }
+
 
   // Populate table at the start and then listen for submit click events for further events.
   populateTable();
